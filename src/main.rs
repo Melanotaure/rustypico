542f32d4534405dda948a15e22b2e4f51f002e74
unknown <luc.gaborit@gmail.com>
2025-03-31 01:05:45 +0200

    blinky with hal

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,53 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::{read_volatile, write_volatile};
-use cortex_m::asm::nop;
-use cortex_m_rt::entry;
 use panic_halt as _;
 
+use hal::pac;
 use rp2040_boot2;
+use rp2040_hal as hal;
+
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::{OutputPin, StatefulOutputPin};
 
 #[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-#[entry]
+const XTAL_FREQ_HZ: u32 = 12_000_000; // 12MHz
+
+#[rp2040_hal::entry]
 fn main() -> ! {
-    const RESET_CLR: *mut u32 = 0x4000_F000 as *mut u32;
-    unsafe {
-        write_volatile(RESET_CLR, 0x0000_0020); // Clear the reset bit
-    }
+    let mut pac = pac::Peripherals::take().unwrap();
 
-    const RESET_RW: *mut u32 = 0x4000_C008 as *mut u32;
-    unsafe {
-        loop {
-            let rst = read_volatile(RESET_RW); // Read the reset bit
-            if rst & 0x0000_0020 != 0 {
-                break; // Exit the loop if the reset bit is cleared
-            }
-        }
-    }
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    const IO_BANK0_BASE: u32 = 0x40014000;
-    const GPIO25_CTRL: *mut u32 = (IO_BANK0_BASE + 0x0000_00CC) as *mut u32; // GPIO 25 Control Register
-    unsafe {
-        *GPIO25_CTRL = 0x05;
-    }
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
 
-    const GPIO_OE_SET: *mut u32 = 0xd0000024 as *mut u32; // GPIO Output Enable Register
-    const GPIO_OUT_SET: *mut u32 = 0xd0000014 as *mut u32; // GPIO Output Set Register
-    const GPIO_OUT_XOR: *mut u32 = 0xd000001C as *mut u32; // GPIO Output XOR Register
+    let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-    unsafe {
-        // Set GPIO 25 as output
-        write_volatile(GPIO_OE_SET, 1 << 25); // Set GPIO 25 as output
-        write_volatile(GPIO_OUT_SET, 1 << 25);
-    }
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = rp2040_hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    led_pin.set_high().unwrap();
 
     loop {
-        // Toggle GPIO 25
-        unsafe {
-            write_volatile(GPIO_OUT_XOR, 1 << 25); // Toggle the GPIO state
-        }
-        for _ in 0..100_000 {
-            nop();
-        }
+        led_pin.toggle().unwrap();
+        timer.delay_ms(500u32);
     }
 }
